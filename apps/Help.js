dca3502bb0179701d608eb78b442cba45913e7d3
兰罗摩 <3584075812@qq.com>
2023-01-13 10:53:53 +0000

    update apps/Help.js.
    Signed-off-by: 兰罗摩 <3584075812@qq.com>

--- apps/Help.js
+++ apps/Help.js
@@ -5,7 +5,6 @@ import { segment } from "oicq";
 import puppeteer from '../../../lib/puppeteer/puppeteer.js'
 import cfg from '../../../lib/config/config.js'
 import { Cfg, Common, Data, Version, Plugin_Name, Plugin_Path } from '../components/index.js'
-// import Theme from './help/theme.js'
 
 export class RGznbot extends plugin {
   constructor() {
