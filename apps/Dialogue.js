717ae8b140f96e82a1938713ddfe7f376d876f48
JD <ngj291457@163.com>
2023-01-14 04:20:39 +0000

    解决对话异常中断问题
    Signed-off-by: JD <ngj291457@163.com>

--- apps/Dialogue.js
+++ apps/Dialogue.js
@@ -47,7 +47,7 @@ export class RGznbot extends plugin {
         let bot_name =  settings.bot_name 
         let API_token = settings.API_token 
         let model = settings.model          
-        let max_tokens = settings.max_tokens
+        let max_tokens = settings.max_token
         let temperature = settings.temperature   
         let top_p = settings.top_p         
         let top_k = settings.top_k            
@@ -111,7 +111,7 @@ export class RGznbot extends plugin {
 		let bot_name = settings.bot_name
 		let API_token = settings.API_token
 		let model = settings.model
-		let max_tokens = settings.max_tokens
+		let max_tokens = settings.max_token
 		let temperature = settings.temperature
 		let top_p = settings.top_p
 		let top_k = settings.top_k
