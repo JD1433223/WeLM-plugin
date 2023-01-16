4ecf97734255221dc78c3dac28ec2ecfbc9460cc
兰罗摩 <3584075812@qq.com>
2023-01-16 20:59:15 +0000

    1
    Signed-off-by: 兰罗摩 <3584075812@qq.com>

---
+++ components/index.js
@@ -0,0 +1,7 @@
+import Data from './Data.js'
+import Common from './Common.js'
+import Cfg from './Cfg.js'
+import Version from './Version.js'
+import App from './App.js'
+
+export { Data, Cfg, Common, Version, App }
