15efa36dba92cf314a0b76212080e2eb4f6b5975
兰罗摩 <3584075812@qq.com>
2023-01-17 15:39:07 +0800

    1

--- apps/Help.js
+++ apps/Help.js
@@ -8,8 +8,6 @@ export class RGznbot extends plugin {
     super({
       /** 功能名称 */
       name: '帮助',
-      /** 功能描述 */
-      dsc: '',
       /** https://oicqjs.github.io/oicq/#events */
       event: 'message',
       /** 优先级，数字越小等级越高 */
