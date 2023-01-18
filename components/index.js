eccedcbd4675178a29bb2f771767533c9bf99566
兰罗摩 <3584075812@qq.com>
2023-01-19 00:13:01 +0800

    1

--- components/index.js
+++ components/index.js
@@ -2,6 +2,5 @@ import Data from './Data.js'
 import Common from './Common.js'
 import Cfg from './Cfg.js'
 import Version from './Version.js'
-import App from './App.js'
 
-export { Data, Cfg, Common, Version, App }
+export { Data, Cfg, Common, Version }
