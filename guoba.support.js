85e622e08ea2c282eb3bbeca6674b68ac343f4cc
兰罗摩 <3584075812@qq.com>
2023-03-14 12:50:51 +0000

    update guoba.support.js.
    Signed-off-by: 兰罗摩 <3584075812@qq.com>

--- guoba.support.js
+++ guoba.support.js
@@ -8,14 +8,14 @@ export function supportGuoba() {
     // 如果你的插件没有在插件库里，那么需要填上补充信息
     // 如果存在的话，那么填不填就无所谓了，填了就以你的信息为准
     pluginInfo: {
-      name: 'WeLM-Plugin',
-      title: 'WeLM-plugin',
+      name: 'WeLM-plugin',
+      title: 'WeLM自定义对话(WeLM Plugin)',
       author: '@书辞千楪',
       authorlink: 'https://gitee.com/shuciqianye',
       link: 'https://gitee.com/shuciqianye/yunzai-custom-dialogue-welm',
       isV3: true,
       isV2: false,
-      description: '提供个性ai问答和提问(chatgpt下下下位替代)功能',
+      description: '提供个性ai问答、提问与续写等功能(chatgpt下下下位替代)功能',
       icon: 'eos-icons:ai-operator'
     },
     // 配置项信息
